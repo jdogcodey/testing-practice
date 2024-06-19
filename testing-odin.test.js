334529205f4a990cce516b35ba5fdacaf4834f1c
Jonathan Dawson <Jonathan@Jonathans-MacBook.local>
2024-06-19 20:28:05 +0100

    Written test for reverse string

--- testing-odin.test.js
+++ testing-odin.test.js
@@ -3,3 +3,7 @@ import { capitalize } from "./testing-odin.js";
 test("capitalize function", () => {
   expect(capitalize("test")).toBe("Test");
 });
+
+test("reverse string function", () => {
+  expect(reverseString("reverse this string")).toBe("gnirts siht esrever");
+});
